d8d2f997dba78e271dce3eb38275b0e7b60fde1c
Reeka <reekamaharaj@gmail.com>
2020-04-16 18:39:01 -0500

    added some functionality for choosing an item to purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,10 +6,10 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "yuffie",
-    database: "bamazonDB"
+    database: "bamazonDB",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     table();
@@ -18,29 +18,50 @@ connection.connect(function(err) {
 //function to print table
 /*
 run
-[ ] display all items available for sale 
-    [ ] displays item ids
-    [ ] displays item name
-    [ ] displays item price
+[x] display all items available for sale 
+    [x] displays item ids
+    [x] displays item name
+    [x] displays item price
 */
-function table(){
-    connection.query("SELECT * from products",
-    function(err, res){
+function table() {
+    connection.query("SELECT * from products", function (err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.log("The available items are:");
+        res.forEach((res) => {
+            console.log(
+                `id: ${res.item_id} product: ${res.product_name} price: $${res.price} number in stock: ${res.stock_quantity}`
+            );
+        });
+        shop();
     });
 }
 
 /*
 prompt user with messages
-[ ] Ask user for the id of the product they want
+[x] Ask user for the id of the product they want
 [ ] Ask user how many units they want to buy
 [ ] Confirm purchase
 */
-function shop(){
-
+function shop() {
+    inquirer
+        .prompt({
+            name: "item_id",
+            type: "number",
+            message: "Input the item number you wish to purchase.",
+        })
+        .then(function (response) {
+            let query = "SELECT item_id, product_name, price FROM products WHERE ?";
+            connection.query(query, { item_id: response.item_id }, function (err, res) {
+                    if (err) throw err;
+                    for (var i = 0; i < res.length; i++) {
+                        console.log( "The following information is for the selected item. \n id: " + res[i].item_id + " product: " + res[i].product_name + " price: $" + res[i].price + "number in stock: " + parseInt(res[i].stock_quantity));
+                    }
+                    purchase();
+            });
+        });
 }
+
+//question shows up before the table and the input is after the table, assuming this is because the function for the question starts running before the table has been populated. Need to fix that
 
 /*
 [ ] Check database to see if the item is available
@@ -53,10 +74,20 @@ If No
     [ ] 'Insufficient quantity' if there are not enough of the item
     [ ] Ends transaction
 */
-function checkDb(){
 
-}
-
-function purchase(){
-
+function purchase() {
+    inquirer.prompt({
+        name: "amount",
+        type: "number",
+        message: "How many would you like to purchase?"
+    }).then(function(response){
+        let request = response.amount;
+        let query = "SELECT stock_quantity FROM products";
+        connection.query(query, function(err, res){
+            if (err) throw err;
+            // console.log("Number in stock: " + res.stock_quantity);
+            console.log("in stock: " + parseInt(res.stock_quantity));
+            console.log("You want: " + request);
+        })
+    })
 }
